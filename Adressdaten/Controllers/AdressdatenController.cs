e46d99fde292062c6452dc27ede237135b42b7c0
Pascal Zöller <UI126735@rwe.com>
2020-03-27 15:28:14 +0100

    Projektdateien hinzufügen.

---
+++ Adressdaten/Controllers/AdressdatenController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Adressdaten.Models;
+
+namespace Adressdaten.Controllers
+{
+    [Route("api/adressdaten")]
+    [ApiController]
+    public class AdressdatenController : ControllerBase
+    {
+        private readonly AdressdatenContext _context;
+
+        public AdressdatenController(AdressdatenContext context)
+        {
+            _context = context;
+
+            if (_context.AdressdatenItems.Count() == 0)
+            {
+                // Create a new AdressdatenItem if collection is empty,
+                // which means you can't delete all AdressdatenItems.
+                _context.AdressdatenItems.Add(new AdressdatenItem { Name = "Item1" });
+                _context.AdressdatenItems.Add(new AdressdatenItem { Name = "Item2" });
+                _context.AdressdatenItems.Add(new AdressdatenItem { Name = "Item3", IsComplete = true });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdressdatenItem>>> GetAdressdatenItems()
+        {
+            return await _context.AdressdatenItems.ToListAsync();
+        }
+
+        // GET: api/Adressdaten/5
+        [HttpGet("plz={id}&ort={name}")]
+        public async Task<ActionResult<IList<AdressdatenItem>>> GetAdressdatenItem(long id, string name)
+        {
+            var AdressdatenItem = _context.AdressdatenItems.Where(a => a.Id == id || a.Name.Contains(name));
+            return await AdressdatenItem.ToListAsync();
+        }
+    }
+}
---
+++ Adressdaten/wwwroot/site.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "api/adressdaten/plz=4&ort=tem",
+        cache: false,
+        success: function (data) {
+            console.log(data);
+            return data;
+        }
+    })
+})
